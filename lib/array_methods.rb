82f8058964a251e3ca0da2b3f058ba20bddab6a7
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-25 20:23:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,12 @@
 def find_element_index(array, value_to_find)
   i=0 
+  found_number = nil 
   while array[i] do 
     if array[i] == value_to_find 
-      puts array[i] 
-    else 
-      puts nil 
+      array[i] = found_number
     i = i + 1 
   end
+  return found_number 
 end
 
 def find_max_value(array)
